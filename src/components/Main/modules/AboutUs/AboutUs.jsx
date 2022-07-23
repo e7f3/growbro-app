6ad96185869cf68f5bcaed9a391851c8c2da355e
ilya.m@leadsforfinance.com <ilya.m@leadsforfinance.com>
2022-07-23 22:54:11 +0400

    Fixed about us section bottom image

--- src/components/Main/modules/AboutUs/AboutUs.jsx
+++ src/components/Main/modules/AboutUs/AboutUs.jsx
@@ -5,7 +5,6 @@ import Container from '../../../Container/Container'
 import MainInfo from './components/MainInfo/MainInfo'
 import OurBenefits from './components/OurBenefits/OurBenefits'
 import SecondaryInfo from './components/SecondaryInfo/SecondaryInfo'
-import Test from '../../../../assets/images/test.png'
 
 export default function AboutUs() {
   return (
@@ -17,7 +16,7 @@ export default function AboutUs() {
           <SecondaryInfo />
         </div>
       </Container>
-      <div className='test'></div>
+      <div className='about-us__bottom-img' />
     </section>
   )
 }
